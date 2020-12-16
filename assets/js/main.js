703908d108ca3fe9cc2c056fb1e9225088dd3378
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2020-12-16 20:39:17 +0100

    add task

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,25 @@
 let app = new Vue ({
-    el: "#app"
-})
+    el: "#app",
+    data : {
+        logoBoolean : "./assets/img/logoBoolean.png",
+        task : "",
+        tasks: [
+
+        ]
+    },
+    created() {
+        document.addEventListener("keyup", (e) => {
+          if (e.key === "Enter") {
+            this.addTask();
+          }
+        });
+    },      
+    methods : {
+        addTask(){
+            this.tasks.push(this.task);
+        },
+        removeTask(index){
+            this.tasks.splice(index, 1);
+        }
+    }
+});
